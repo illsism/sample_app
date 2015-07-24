6275980d2e61f5638ca92c882468683cb540e394
illsism <raul.unknown@gmail.com>
2015-07-24 16:55:50 +0300

    Create login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,10 +5,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, user: { name: "",
-                               email: "invalid@email",
-                               password:              "one",
-                               password_confirmation: "two"  }
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
     end
     assert_template 'users/new'
     assert_select 'div#<CSS id for error explanation>'
@@ -23,7 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password:              "password",
                                             password_confirmation: "password" }
     end
-  assert_template 'users/show'
-  assert_not flash.empty?
+    assert_template 'users/show'
   end
 end
